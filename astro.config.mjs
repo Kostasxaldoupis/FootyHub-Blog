93d799f18eb05c6e4e571d499869d506cf60550a
Kostas <kostasxaldoupis@gmail.com>
2026-06-05 14:03:53 +0300

    World Cup Blog + some logos from before

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
+// No adapter for static export
 export default defineConfig({
+  output: 'static',
   integrations: [react()],
-
   vite: {
     plugins: [tailwindcss()]
   }
